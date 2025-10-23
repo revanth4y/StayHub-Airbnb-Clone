d0fb22d1cc9aae1a44f8ae6a75eb51bc5c42ae3b
Revanth Y <revanth.cse.rymec@gmail.com>
2025-10-23 22:02:00 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+const { object } = require("joi");
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/StayHub";
+
+main()
+   .then(() => {
+  console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main(){
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async() => {
+    await Listing.deleteMany({});
+     initData.data = initData.data.map((obj) => ({...obj, owner: "68f905b6cfdb3b0a57bb6517"}));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
+
+initDB(); 
+
---
+++ routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
+const usersController = require("../controllers/users.js");
+const { render } = require("ejs");
+const user = require("../models/user.js");
+
+router
+   .route("/signup")
+   .get(usersController.renderSignupForm)
+   .post(wrapAsync(usersController.signup));
+
+router.route("/login")
+.get(usersController.renderLoginForm)
+.post(
+    saveRedirectUrl, 
+    passport.authenticate("local", { 
+    failureRedirect: "/login", 
+    failureFlash: true 
+ }),
+    usersController.login
+ );
+
+router.get("/logout", usersController.logout);
+    
+module.exports = router;
